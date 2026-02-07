6733d48295205d1518d0f2469e18e3ca0a90d57f
huhongwei <381093478@qq.com>
2026-02-08 00:20:25 +0800

    Add KaTeX CSS for math rendering and new image file
    - Imported KaTeX CSS to enhance math formula rendering capabilities.
    - Added a new image file related to Lishui documentation.
    - Adjusted custom CSS for improved header logo spacing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,23 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightVideos from "starlight-videos";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [
     starlight({
       favicon: "/favicon.ico",
       plugins: [starlightVideos()],
       title: "商密知识库",
+      components: {
+        TableOfContents: './src/components/TableOfContents.astro',
+      },
       locales: {
         root: {
           label: '简体中文',
@@ -27,6 +36,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: "丽水市本级政府投资信息化项目管理办法", slug: "lishui/lishui-gov-it-project-management-rules" },
+            { label: "信息系统商用密码应用成本测算指南", slug: "lishui/commercial-crypto-cost-estimation-guide" },
           ],
         },
         {
